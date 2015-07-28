8b525e32e23aae60332af62c7aa32c6e9fc44a4c
krzysztofwojcicki <krzysztofwojcicki@gmail.com>
2015-07-28 14:12:49 +0200

    Layout, contact, about pages

---
+++ src/AnimalWeb/Controllers/DogeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using System.Diagnostics;
+
+namespace AnimalWeb.Controllers
+{
+    public class DogeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/AnimalWeb/Controllers/HomeController.cs
+++ src/AnimalWeb/Controllers/HomeController.cs
@@ -4,18 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using System.Diagnostics;
-using Animal.Db;
 
 namespace AnimalWeb.Controllers
 {
     public class HomeController : Controller
     {
-        AnimalModel _model;
-        public HomeController(AnimalModel model)
-        {
-            _model = model;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -25,9 +18,7 @@ namespace AnimalWeb.Controllers
         {
             ViewData["Message"] = "Your application description page.";
 
-            var d = _model.Dogs.FirstOrDefault();
-
-            return View(d);
+            return View();
         }
 
         public IActionResult Contact()
